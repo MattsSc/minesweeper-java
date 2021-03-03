174485deaac94ee3fa15fc1f05585d98a59a3dcd
Matias Scandroglio <matias.scandroglio@almundo.com>
2021-03-02 21:40:06 -0300

    add query to get games by user id

--- src/main/java/com/mattssc/minesweeper/domain/repository/GameRepository.java
+++ src/main/java/com/mattssc/minesweeper/domain/repository/GameRepository.java
@@ -3,7 +3,16 @@ package com.mattssc.minesweeper.domain.repository;
 import com.mattssc.minesweeper.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+
+    @Query(value = "SELECT game FROM Game game WHERE game.user.id = :userId")
+    List<Game> findByUser(@Param("userId") Long userId);
+
+
 
 }
